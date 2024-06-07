4af61eb4ad74b8122d547772168b5977dbb7b68e
Rosita Ananza <rositaanannza17@gmail.com>
2024-06-07 22:30:58 +0700

    menambahkan migration

---
+++ database/migrations/2024_06_07_152718_create_profil_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('profil', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama', 45);
+            $table->date('ttl');
+            $table->string('gender', 45);
+            $table->string('alamat', 45);
+            $table->unsignedBigInteger('users_id');
+            $table->foreign('users_id')->references('id')->on('users')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('profil');
+    }
+};

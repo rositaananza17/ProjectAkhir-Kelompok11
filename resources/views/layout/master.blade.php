c9f7901f117c99cf666d8d1aaaa3f36efa476a95
Rosita Ananza <rositaanannza17@gmail.com>
2024-06-08 18:57:17 +0700

    memperbaiki template gallery

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -44,18 +44,12 @@
                 <!-- Collect the nav links, forms, and other content for toggling -->
                 <div class="collapse navbar-collapse offset" id="navbarSupportedContent">
                     <ul class="nav navbar-nav menu_nav ml-auto">
-<<<<<<< HEAD
-                        <li class="nav-item active"><a class="nav-link" href="/#">Home</a></li> 
-=======
                         <li class="nav-item active"><a class="nav-link" href="/">Home</a></li> 
->>>>>>> a19bc7eb39d39e6840011b547761ff17521d98da
                        
-                       
-                        <li class="nav-item"><a class="nav-link" href="">Gallery</a></li>
                         <li class="nav-item submenu dropdown">
                             <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Blog</a>
                             <ul class="dropdown-menu">
-                                <li class="nav-item"><a class="nav-link" href="blog.html">Blog</a></li>
+                                <li class="nav-item"><a class="nav-link" href="/">Blog</a></li>
                                 <li class="nav-item"><a class="nav-link" href="blog-single.html">Blog Details</a></li>
                             </ul>
                         </li> 
